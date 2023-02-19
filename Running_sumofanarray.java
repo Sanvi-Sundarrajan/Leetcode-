9b92168060b14ec398394910011b7c7a7365e35e
Sanvi-Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2023-02-19 11:06:52 +0530

    Update Running_sumofanarray.java

--- Running_sumofanarray.java
+++ Running_sumofanarray.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+class Running_sumofanarray {
     public int[] runningSum(int[] nums) {
         for(int i = 1 ; i < nums.length ; i++) {
       nums[i] = nums[i] + nums[i-1];   
@@ -9,7 +9,7 @@ class Main {
     
     public static void main(String args [])
     {
-        Main s = new Main();
+        Running_sumofanarray s = new Running_sumofanarray();
         int nums []= new int [];
         Scanner sc = new Scanner(System.in);
         System.out.println("Input: ");
